480fc3eff26b16f0354e4f961ab4ecbf38175d0b
Dmitry Kirka <kirka.dev@ya.ru>
2020-11-10 01:26:03 +0300

    sprint 5

--- script/script.js
+++ script/script.js
@@ -1,42 +1,162 @@
-let popup = document.querySelector('.popup-wrapper');
-let popupWindow = document.querySelector('.popup');
-let formElement = document.querySelector('.profile-edit');
-
+// Профиль
+let popupProfileEdit = document.querySelector('#profile-edit');
+let popupProfileEditWindow = document.querySelector('#profile-edit .popup');
+let formProfileEdit = document.querySelector('.profile-edit');
 let btnProfileEdit = document.querySelector('.profile-info__edit-button');
-let btnPopupClose = document.querySelector('.popup__close');
-
+let btnProfileEditClose = document.querySelector('#profile-edit .popup__close');
 let nameField = document.querySelector('.profile-info__title');
 let jobField = document.querySelector('.profile-info__subtitle');
+let nameInput = document.querySelector('#profile-edit__name');
+let jobInput = document.querySelector('#profile-edit__job');
 
-let nameInput = document.querySelector('#profile-edit__title');
-let jobInput = document.querySelector('#profile-edit__subtitle');
-
-function openProfilEdit(){
+function openProfilEdit() {
   nameInput.value = nameField.textContent;
   jobInput.value = jobField.textContent;
-
-  popup.classList.add('popup-wrapper_active');
+  popupProfileEdit.classList.add('popup-wrapper_active');
 }
 
-function closeProfilEdit(){
-  popup.classList.remove('popup-wrapper_active');
+function closeProfilEdit() {
+  popupProfileEdit.classList.remove('popup-wrapper_active');
 }
 
-function formSubmitHandler(event) {
+function formProfileSubmit(event) {
   event.preventDefault();
-
   nameField.textContent = nameInput.value;
   jobField.textContent = jobInput.value;
-
   closeProfilEdit();
 }
 
-popup.addEventListener('click', function(event) {
-  if (!popupWindow.contains(event.target) && popup.classList.contains('popup-wrapper_active')){
+popupProfileEdit.addEventListener('click', function (event) {
+  if (!popupProfileEditWindow.contains(event.target) && popupProfileEdit.classList.contains('popup-wrapper_active')) {
     closeProfilEdit();
   }
 });
 
-formElement.addEventListener('submit', formSubmitHandler);
+formProfileEdit.addEventListener('submit', formProfileSubmit);
 btnProfileEdit.addEventListener('click', openProfilEdit);
-btnPopupClose.addEventListener('click', closeProfilEdit);
+btnProfileEditClose.addEventListener('click', closeProfilEdit);
+
+// Карточки
+let popupCardAdd = document.querySelector('#card-add');
+let popupCardAddWindow = document.querySelector('#card-add .popup');
+let formCardAdd = document.querySelector('.card-add');
+let btnCardAdd = document.querySelector('.profile__add-button');
+let btnCardAddClose = document.querySelector('#card-add .popup__close');
+
+function openCardAdd() {
+  popupCardAdd.classList.add('popup-wrapper_active');
+}
+
+function closeCardAdd() {
+  popupCardAdd.classList.remove('popup-wrapper_active');
+}
+
+popupCardAdd.addEventListener('click', function (event) {
+  if (!popupCardAddWindow.contains(event.target) && popupCardAdd.classList.contains('popup-wrapper_active')) {
+    closeCardAdd();
+  }
+});
+
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('element__delete')) {
+    event.target.parentElement.remove();
+  }
+  if (event.target.classList.contains('element__like')) {
+    event.target.classList.toggle('element__like_active')
+  }
+  if (event.target.classList.contains('element__cover')) {
+    let src = event.target.src;
+    let title = event.target.parentElement.querySelector('.element__title').textContent;
+    imageOpen(src, title);
+  }
+});
+
+btnCardAdd.addEventListener('click', openCardAdd);
+btnCardAddClose.addEventListener('click', closeCardAdd);
+
+const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardTemplate = document.querySelector('#card').content;
+const cards = document.querySelector('.elements');
+
+initialCards.forEach(function (item) {
+  let card = cardTemplate.cloneNode(true);
+  card.querySelector('.element__title').textContent = item.name;
+  card.querySelector('.element__cover').src = item.link;
+  cards.prepend(card);
+});
+
+let titleValue = document.querySelector('#card-add__title');
+let imgValue = document.querySelector('#card-add__img');
+
+function formProfileSubmit(event) {
+  event.preventDefault();
+
+  let card = cardTemplate.cloneNode(true);
+  card.querySelector('.element__title').textContent = titleValue.value;
+  card.querySelector('.element__cover').src = imgValue.value;
+  titleValue.value = '';
+  imgValue.value = '';
+  cards.prepend(card);
+
+  closeCardAdd();
+}
+
+formCardAdd.addEventListener('submit', formProfileSubmit);
+
+// Изображение (попап)
+
+let popupImage = document.querySelector('#popup-image');
+let popupImageWindow = document.querySelector('#popup-image .popup-image');
+let popupImageTitle = document.querySelector('#popup-image .popup-image__title');
+let btnImageClose = document.querySelector('#popup-image .popup__close');
+
+function imageOpen(src, title) {
+  let image = new Image;
+  popupImageTitle.textContent = title;
+
+  if (popupImageWindow.querySelector('.popup-image__image')) {
+    popupImageWindow.querySelector('.popup-image__image').remove();
+  }
+
+  image.classList.add('popup-image__image')
+  image.src = src;
+  popupImageWindow.prepend(image);
+  popupImage.classList.add('popup-wrapper_active');
+}
+
+function imageClose() {
+  popupImage.classList.remove('popup-wrapper_active');
+}
+
+popupImage.addEventListener('click', function (event) {
+  if (!popupImageWindow.contains(event.target) && popupImage.classList.contains('popup-wrapper_active')) {
+    imageClose();
+  }
+});
+
+btnImageClose.addEventListener('click', imageClose);
